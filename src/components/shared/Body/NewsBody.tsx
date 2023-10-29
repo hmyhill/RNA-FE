180be42afc2ddda73e90f3bfe20cc97d6619abb9
ScottE2 <elliott.scott@vodafone.com>
2023-10-29 13:34:41 +0000

    Finshed Main Content & Improved Responsiveness - Created component allowing stories to be mapped into body grid - Started working on the header to improve it's responsiveness

--- src/components/shared/Body/NewsBody.tsx
+++ src/components/shared/Body/NewsBody.tsx
@@ -1,8 +1,8 @@
 import {
     Box,
+    Grid,
     Typography
 } from "@mui/material";
-
 import { 
     createTheme,
     ThemeProvider,
@@ -21,9 +21,11 @@ export default function Body(props: BodyProps){
 
     /* NEED TO ADD REQUEST TO GET STORIES RELEVANT TO PAGE NAME */
 
+    let data: string[] = ["Test to see if this works", "story2", "story3"]
+
     return (
         <>
-            <TickerComponent data={["Test to see if this works", "story2"]} />
+            <TickerComponent data={data} />
             <ThemeProvider theme = {theme}>
                 <Box id="MainStory" sx = {{ display: "flex", margin: "2%", height: "50vh", color: "black", border: "2px solid black" }}>
                     <Box sx={{ backgroundColor: props.backgroundColour, width: "38%", height: "100%", marginRight: "2%", borderRight: "2px solid black"}}>
@@ -41,8 +43,27 @@ export default function Body(props: BodyProps){
                     </div>
                 </Box>
 
-                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: props.backgroundColour, height: "10vh", marginLeft: "5%", marginRight: "5%", border: "2px solid black", borderRadius: "3vw"}}>
+                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: props.backgroundColour, height: "10vh", margin: "2%", margintTop: "0%", border: "2px solid black", borderRadius: "3vw"}}>
                     <Typography variant={"h4"} > Other {props.pageName} Stories For You </Typography>
+                </Box>
+
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container columns= {12} rowSpacing={1} columnSpacing={2} margin="0px" width="100%">
+                        {data.map((story, index) => {
+                            return (
+                                <Grid item xs={12} md={6} lg={4} sx={{ padding: "0 !important" }}>
+                                    <Box sx={{ backgroundColor: props.backgroundColour, margin: "2%" }}>
+                                        <Box sx={{ backgroundColor: "white", margin: "2%", height: "25vh", border: "2px solid black"}}>
+                                            
+                                        </Box>
+                                        <Box sx={{ margin: "2%", height: "25vh" }}>
+                                            <Typography variant={"h6"} > {data[index]} </Typography>
+                                        </Box>
+                                    </Box>
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
                 </Box>
             </ThemeProvider>
         </>
