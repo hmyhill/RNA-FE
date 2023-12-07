334b941987d166b36dc7a9d7c5953d028beb0d08
ERB71 <59881107+ERB71@users.noreply.github.com>
2023-12-07 16:12:29 +0000

    Correctly adding new Entertainment colours

--- src/containers/Entertainment/Entertainment.tsx
+++ src/containers/Entertainment/Entertainment.tsx
@@ -4,8 +4,8 @@ import NewsBody from "../../components/shared/Body/NewsBody";
 const Entertainment = () => {
   return (
     <>
-      <Navbar pageName={"entertainment"} backgroundColour={"#0E7A0D"}/>
-      <NewsBody pageName={"Entertainment"} backgroundColour={"rgba(14, 122, 13, 0.25)" } />
+      <Navbar pageName={"entertainment"} backgroundColour={"#4caf50"}/>
+      <NewsBody pageName={"Entertainment"} backgroundColour={"#e8f5e9" } />
     </>
   );
 };
