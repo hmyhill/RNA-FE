460486507a6407471f7d85dd1f6565f8acfa6b33
ERB71 <59881107+ERB71@users.noreply.github.com>
2023-12-09 16:33:31 +0000

    News API integration & Added Sharing Feature (#16)
    * News request implementation and reformatting
    - Initial implementation of the backend news api requests
    - Reformatted frontend to facilitate limitations of the API's responses
    
    * Changes to help with pull request
    
    * Formatting changes to accomodate new API response
    
    * Api Integration Visual Changes 2
    
    * Body variant fix
    
    * Final Major formatting changes
    
    * New Main Header Section
    
    MainMUI header
    
    * Full Integrating MainMUICard with the API
    
    * Visual refinements and MailTo sharing integration
    
    ---------
    
    Co-authored-by: CharlesC <charlesclayton629@gmail.com>

--- src/components/shared/Body/NewsBody.tsx
+++ src/components/shared/Body/NewsBody.tsx
@@ -1,6 +1,5 @@
 import {
     Box,
-    Container,
     Grid,
     Typography
 } from "@mui/material";
@@ -11,10 +10,12 @@ import {
 } from '@mui/material/styles';
 import TickerComponent from "../Ticker/Ticker";
 import MUIcard from "../MUICard/MUIcard";
+import MainMUICard from "../MainMUICard/MainMUICard";
 
 interface BodyProps {
     pageName: string;
     backgroundColour: string;
+    newsStories: any[]
 }
 
 
@@ -25,87 +26,33 @@ export default function Body(props: BodyProps){
             'DM Serif Display',
           ].join(','),
         },});
-    theme = responsiveFontSizes(theme);
-    //Added A new News font to News body pages, if you want to change this
-        //or add additional fonts switch out font in the typeography square brackets[]
+    const responsiveTheme = responsiveFontSizes(theme);
 
-    /* NEED TO ADD REQUEST TO GET STORIES RELEVANT TO PAGE NAME */
-
-    let data: string[] = ["Story 1.", "Story 2.", "Story 3."]
-    
-    //You can place variables with the news paper details in the story 1 -3 sections.
+    const minorStories = props.newsStories.slice(1);
 
     return (
         <>
-            <TickerComponent data={data} />
-            <ThemeProvider theme = {theme}>
-            <Box id="MainStoryWide" sx = {{ display: { xs: "none", sm: "flex" }, margin: "2%", height: {sm: "35vh", md:"45vh" }, color: "black" }}>
-                    <div style = {{ display: "flex", flexDirection: "column", width: "100%" }}>
-                        <Box sx={{ backgroundColor: props.backgroundColour, height: "14%", marginBottom: "1%", border: "1px solid black", borderRadius: "1vw"}}>
-                            <Typography variant={"h4"}> Headline Goes Here </Typography>
-                        </Box>
-                        
-                        <div style = {{ display: "flex", height: "85%" }}>
-                            <Box sx={{ backgroundColor: props.backgroundColour, width: "38%", marginRight: "2%", border: "1px solid black", borderRadius: "0.3vw"}}>
-                                <Typography variant={"h6"}> Image Goes Here </Typography>
-                            </Box>
-                            <Box sx={{ backgroundColor: props.backgroundColour, width: "60%", border: "1px solid black", borderRadius: "0.3vw"}}>
-                                <Typography variant={"h6"}> Article Goes Here </Typography>
-                            </Box>
-                        </div>
-                    </div>
-                </Box>
+            <TickerComponent data={props.newsStories.map(story => story.title)} />
+            <ThemeProvider theme = {responsiveTheme}>
+                <MainMUICard cardColour={props.backgroundColour} story={props.newsStories[0]} />
 
-                <Box id="MainStoryNarrow" sx = {{ display: { xs: "flex", sm: "none" }, flexDirection: "column", margin: "2%", height: "50vh", color: "black", backgroundColor: props.backgroundColour }}>
-                    <Box sx={{ backgroundColor: "#fff", height: "9%", marginBottom: "1%", border: "2px solid black" }}>
-                        <Typography variant={"h3"}> Headline Goes Here </Typography>
-                    </Box>
-                    
-                        <Box sx={{ backgroundColor: "#fff", width: "50%", height: "44%", border: "2px solid black", marginLeft: "auto", marginRight: "auto", marginBottom: "1%" }}>
-                            <Typography variant={"h6"}> Image Goes Here </Typography>
-                        </Box>
-
-                        <Box sx={{ backgroundColor: "#fff", height: "45%", border: "2px solid black" }}>
-                            <Typography variant={"h6"}> Article Goes Here </Typography>
-                        </Box>
-                </Box>
-
-
-                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: props.backgroundColour, height: "10vh", margin: "2%", margintTop: "0%", border: "1px solid black", borderRadius: "3vw"}}>
+                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: props.backgroundColour, marginX: "2%", height: "8vh", border: "1px solid black", borderRadius: "3vw"}}>
                     <Typography variant={"h4"} > Other {props.pageName} Stories For You </Typography>
                 </Box>
 
-                <Box sx={{ flexGrow: 1 }}>
+                <Box sx={{ flexGrow: 1, marginBottom: "2vh"}}>
                     <Grid container columns= {12} rowSpacing={1} columnSpacing={2} margin="0px" width="100%">
-                        {data.map((story, index) => {
+                        {minorStories.map((story) => {
                             return (
-                                <Grid item xs={12} sm={6} md={4} sx={{ padding: "0 !important" }} style={{overflow: 'auto'}}>
-                                    {/*added auto overflow for side scrolling*/}
+                                <Grid item xs={12} sm={6} md={4} sx={{ padding: "0 !important" }} >
                                     <Box sx={{ backgroundColor: props.backgroundColour, margin: "5%" }}>
-                                        <MUIcard cardcolor={[props.backgroundColour]}/>
-                                        {/*This is what is used for the cards at the buttom*/}
+                                        <MUIcard story={story} cardColour={[props.backgroundColour]}/>
+                                        {/*This is what is used for the cards at the bottom of the page*/}
                                     </Box>
                                 </Grid>
                             )
                         })}
                     </Grid>
-                </Box>
-                {/*Footer */}
-                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: props.backgroundColour, height: "10vh", margin: "0%", margintTop: "0%", border: "1px solid black", borderRadius: "0vw"}}>
-                    <Container maxWidth="lg">
-                        <Grid container direction="column" alignItems="center">
-                            <Grid item xs={12}>
-                                <Typography color="black" variant="h5">
-                                    RNA NEWS
-                                </Typography>
-                            </Grid>
-                        <Grid item xs={12}>
-                        <Typography color="textSecondary" variant="subtitle1">
-                            {`${new Date().getFullYear()} | Help | About Us | Contact Us`}
-                        </Typography>
-                            </Grid>
-                        </Grid>
-                    </Container>
                 </Box>
             </ThemeProvider>
         </>
--- src/containers/Sport/Sport.tsx
+++ src/containers/Sport/Sport.tsx
@@ -1,11 +1,36 @@
 import Navbar from "../../components/shared/Navbar/Navbar";
 import NewsBody from "../../components/shared/Body/NewsBody";
+import Footer from "../../components/shared/Footer/Footer";
+import React from "react"
+import { httpGet } from "../../utils/api.utils";
 
 const Sport = () => {
+  const[requestData, setRequestData] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    async function getNews() {
+      try {
+        const news: { data?: { results?: any[] } } = await httpGet(`/news/sport`);
+        return news.data.results || [];
+      } catch (error) {
+        console.error("Error fetching news:", error);
+        return [];
+      }
+    }
+  
+    async function fetchData() {
+      const data = await getNews();
+      setRequestData(data);
+    }
+  
+    fetchData();
+  }, []);
+
   return (
     <>
       <Navbar pageName={"sport"} backgroundColour={"#ff9800"} />
-      <NewsBody pageName={"Sports"} backgroundColour={"#fff3e0" } />
+      <NewsBody pageName={"Sports"} newsStories={requestData} backgroundColour={"#fff3e0" } />
+      <Footer backgroundColour={"#fff3e0"} />
     </>
   );
 };
