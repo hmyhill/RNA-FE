32ec6dcde36be112041e73865b5198ddbb13e7c0
elliot-fenwick <elliot@fenwickfamily.co.uk>
2023-12-10 17:54:43 +0000

    Repointing all news API requests to news API

--- src/containers/Tech/Tech.tsx
+++ src/containers/Tech/Tech.tsx
@@ -1,35 +1,41 @@
 import Navbar from "../../components/shared/Navbar/Navbar";
 import NewsBody from "../../components/shared/Body/NewsBody";
 import Footer from "../../components/shared/Footer/Footer";
-import React from "react"
+import React from "react";
 import { httpGet } from "../../utils/api.utils";
 
 const Tech = () => {
-  const[requestData, setRequestData] = React.useState<any[]>([]);
+  const [requestData, setRequestData] = React.useState<any[]>([]);
 
   React.useEffect(() => {
     async function getNews() {
       try {
-        const news: { data?: { results?: any[] } } = await httpGet(`/news/tech`);
+        const news: { data?: { results?: any[] } } = await httpGet(
+          `http://localhost:5000/news/tech`
+        );
         return news.data.results || [];
       } catch (error) {
         console.error("Error fetching news:", error);
         return [];
       }
     }
-  
+
     async function fetchData() {
       const data = await getNews();
       setRequestData(data);
     }
-  
+
     fetchData();
   }, []);
 
   return (
     <>
-      <Navbar pageName={"tech"} backgroundColour={"#009688"}/>
-      <NewsBody pageName={"Tech"} newsStories={requestData} backgroundColour={"#e0f2f1" } />
+      <Navbar pageName={"tech"} backgroundColour={"#009688"} />
+      <NewsBody
+        pageName={"Tech"}
+        newsStories={requestData}
+        backgroundColour={"#e0f2f1"}
+      />
       <Footer backgroundColour={"#e0f2f1"} />
     </>
   );
